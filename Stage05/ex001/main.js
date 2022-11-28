fd1d4c8310f445fb8b7533483f1ef91d8df93407
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2022-11-28 09:10:05 -0300

    att ex

--- Stage05/ex001/main.js
+++ Stage05/ex001/main.js
@@ -1,2 +1,22 @@
-import message from './utils.js'
-console.log(message)
+//import {double, today} from './utils.js'
+//console.log(double(3.5))
+//console.log(today)
+
+//import message from './utils.js'
+//console.log(message)
+
+//import funcaoPadrao, {date, greeting} from './utils.js'
+//funcaoPadrao('Diego')
+
+//import {sum as soma} from './utils.js'
+//console.log(soma(18, 2))
+
+import * as calculator from './utils.js'
+console.log(calculator.subtract(49, 4))
+console.log(calculator.sum(49, 4))
+console.log(calculator.multiply(49, 4))
+console.log(calculator.divide(49, 4))
+
+// pode retornar funções
+// apenas um 'defaut' por arquivo
+// para exporta mais de um objeto: {nome do objeto(const, let, var) igual esta no arquivo fonte}
--- Stage05/ex001/utils.js
+++ Stage05/ex001/utils.js
@@ -1,3 +1,25 @@
-const message = "Texto qualquer"
+//export const today = Date.now()
 
-export default message
+//const message = "Texto qualquer"
+
+//export default message
+
+//const double = number => number * 2
+//export {double}
+
+/*const date = new Date()
+const greeting = name => `hello ${name}`
+export default function (argument) {
+    console.log(date, greeting(argument))
+}
+export {date, greeting}*/
+
+/*export function sum(a, b) {
+    return a + b
+}*/
+
+const sum = (a, b) => a + b
+const multiply = (a, b) => a * b
+const subtract = (a, b) => a - b
+const divide = (a, b) => a / b
+export {sum, multiply, subtract, divide}
