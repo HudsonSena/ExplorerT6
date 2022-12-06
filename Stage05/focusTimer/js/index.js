4956268af436ae0ecc0a5f9384a81bdf809d4641
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2022-12-06 15:20:33 -0300

    att

--- Stage05/focusTimer/js/index.js
+++ Stage05/focusTimer/js/index.js
@@ -1,41 +1,16 @@
+import resetControls from "./controls.js"
+import "./timer"
+
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
-let minutes
+let minutes = Number(minutesDisplay.textContent)
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
-
-function countdown() {
-    setTimeout(function() {
-        let seconds = Number(secondsDisplay.textContent)
-        let minutes = Number(minutesDisplay.textContent)
-
-        secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
-
-        if( minutes <= 0 && seconds <= 1) {
-            buttonPlay.classList.remove('hide')
-            buttonPause.classList.add('hide')
-            buttonSet.classList.remove('hide')
-            buttonStop.classList.add('hide')
-
-            return                       
-        }
-
-        if( seconds <= 0 ) {
-            seconds = 10
-
-            minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
-        }
-        
-        secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
-        
-        countdown()
-
-    }, 1000)
-}
+let timerTimeOut
 
 buttonPlay.addEventListener('click', function() {
     buttonPlay.classList.add('hide')
@@ -50,13 +25,12 @@ buttonPlay.addEventListener('click', function() {
 buttonPause.addEventListener('click', function() {
     buttonPause.classList.add('hide')
     buttonPlay.classList.remove('hide')
+    clearTimeout(timerTimeOut)
 })
 
 buttonStop.addEventListener('click', function() {
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonSet.classList.remove('hide')
-    buttonStop.classList.add('hide')
+    resetControls()
+    resetTimer()
 })
 
 buttonSoundOn.addEventListener('click', function() {
@@ -70,6 +44,12 @@ buttonSoundOff.addEventListener('click', function() {
 })
 
 buttonSet.addEventListener('click', function() {
-    minutes = prompt('Quantos minutos?')
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    let newMinutes = prompt('Quantos minutos?')
+    if(!newMinutes) {
+        resetTimer()
+        return
+    }
+
+    minutes = newMinutes
+    updateTimerDisplay(minutes, 0)
 })
