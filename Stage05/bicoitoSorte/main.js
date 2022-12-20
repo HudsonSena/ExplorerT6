28165e02acb583635bc3b22deff6c9c8e30e5c95
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2022-12-20 17:00:55 -0300

    att

---
+++ Stage05/bicoitoSorte/main.js
@@ -0,0 +1,47 @@
+const buttonBiscoitofechado = document.querySelector('.bFechado')
+const buttonAbrirOutroBiscoito = document.querySelector('.btnAbrirOutroBiscoito')
+const biscoitoFechado = document.querySelector('.biscoitoFechado')
+const biscoitoAberto = document.querySelector('.biscoitoAberto')
+
+const displayTransMessage = document.querySelector('.message')
+const displayMessage = document.querySelector('.textmessage')
+
+const messages = [
+    'Seu dia vai ser incrível',
+    'dia chato', 
+    'dia bom', 
+    'dia ruim', 
+    'dia comum', 
+    'dia qualquer', 
+    'sorte grande', 
+    'sem sorte',
+    'poxa vida',
+    'porra',
+    'chatopra caralho']    
+
+buttonBiscoitofechado.addEventListener('click', biscoitoDaSorte)
+window.addEventListener('keypress', biscoitoDaSorte)
+
+buttonAbrirOutroBiscoito.addEventListener('click', abrirOutroBiscoito)
+window.addEventListener('keydown', abrirOutroBiscoito)
+
+let message = 'Jaboti'
+
+function messageSorteio() {
+    message = messages[(Math.round(Math.random() * 10))]
+}
+
+function biscoitoDaSorte() {
+    biscoitoFechado.classList.add('hide')
+    biscoitoAberto.classList.remove('hide')
+    displayTransMessage.classList.add('transitionMessage')
+
+    messageSorteio()
+
+    displayMessage.textContent = String(message)
+}
+
+function abrirOutroBiscoito() {
+    biscoitoFechado.classList.remove('hide')
+    biscoitoAberto.classList.add('hide')
+}
