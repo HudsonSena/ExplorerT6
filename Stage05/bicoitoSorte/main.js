0835fe963199939f1729c757e62770a884228012
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2022-12-21 11:02:50 -0300

    att para upload

--- Stage05/bicoitoSorte/main.js
+++ Stage05/bicoitoSorte/main.js
@@ -7,51 +7,45 @@ const displayTransMessage = document.querySelector('.message')
 const displayMessage = document.querySelector('.textmessage')
 
 const messages = [
-    'Seu dia vai ser incrível',
-    'dia chato', 
-    'dia bom', 
-    'dia ruim', 
-    'dia comum', 
-    'dia qualquer', 
-    'sorte grande', 
-    'sem sorte',
-    'poxa vida',
-    'porra',
-    'chatopra caralho']    
+    'Seu dia vai ser incrível!',
+    'Aposta na mega sena...', 
+    'Sempre haverá um novo dia.', 
+    'Continue a nadar', 
+    'Dia super normal né, mas amanhã vai ser show!', 
+    'Dia qualquer, mas eu posso mudalo', 
+    'Você é uma pessoa incrível, mas não seja besta.', 
+    'Hoje ta complicado, mas amanhã é um novo dia',
+    'Poxa vida, você é foda pra caralho!',
+    'Que vida difícil em? Tenho que mudar minhas atitudes.',
+    'Hoje ta chato, vamos pular este dia']    
 
 buttonBiscoitofechado.addEventListener('click', biscoitoDaSorte)
 window.addEventListener('keypress', biscoitoDaSorte)
-
 buttonAbrirOutroBiscoito.addEventListener('click', abrirOutroBiscoito)
-window.addEventListener('keydown', abrirOutroBiscoito)
 
 let message = 'Jaboti'
 let codicao = 1
 
-function messageSorteio() {
-    message = messages[(Math.round(Math.random() * 10))]
-}
-
 function biscoitoDaSorte() {
     if(codicao == 2) {
         alert('Abra outro biscoito!')
+        abrirOutroBiscoito()
         codicao = 1
     } else {
         biscoitoFechado.classList.add('hide')
         biscoitoAberto.classList.remove('hide')
         displayTransMessage.classList.add('transitionMessage')
 
-        messageSorteio()
+        message = messages[(Math.round(Math.random() * 10))]
 
         displayMessage.textContent = String(message)
-        
+
         codicao = 2
     }
-
-    
 }
 
 function abrirOutroBiscoito() {
     biscoitoFechado.classList.remove('hide')
     biscoitoAberto.classList.add('hide')
+    codicao = 1
 }
