a2baefaac22f9d036b60bf468019bd07eebb7d08
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-01-12 16:56:26 -0300

    att to home

--- Stage06/ex002/github-Favorites/js/favorites.js
+++ Stage06/ex002/github-Favorites/js/favorites.js
@@ -18,6 +18,45 @@ export class FavoritesView extends Favorites {
         this.removeAllTr()
     }
 
+    [
+        {
+            login: 'hudsonsena',
+            name: "Hudson Sena",
+            public_repos: '76',
+            followers: '120000'
+        },
+        {
+            login: 'maykbrito',
+            name: "Mayk Brito",
+            public_repos: '76',
+            followers: '120000'
+        }
+    ]
+
+    createRow() {
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+            <td class="user">
+                <img src="" alt="">
+                <a href="">
+                    <p>Oliveira Sena</p>
+                    <span>oliveirasena</span>
+                </a>
+            </td>
+
+            <td class="repositories">
+                76
+            </td>
+
+            <td class="foloowers">
+                9589
+            </td>
+            
+            <td><button class="remove">&times;</button></td>
+        `
+        return tr
+    }
+
     removeAllTr() {
         const tbody = this.root.querySelector('table tbody')
 
