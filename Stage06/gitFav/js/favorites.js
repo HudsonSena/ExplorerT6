e5ee7ac154d9aa868415b8c3bc9c94fa6ff4ac84
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-02-15 16:58:20 -0300

    att entries in dom

--- Stage06/gitFav/js/favorites.js
+++ Stage06/gitFav/js/favorites.js
@@ -1,6 +1,24 @@
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
+    }
+
+    load() {
+        this.entries = [
+            {
+                login: 'maykbrito',
+                name: "Mayk Brito",
+                public_repos: '76',
+                followers: '120000'
+            },
+            {
+                login: 'HudsonSena',
+                name: "Hudson Diego",
+                public_repos: '200',
+                followers: '200000'
+            }
+        ]
     }
 }
 
@@ -8,20 +26,31 @@ export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
 
+        this.tbody = this.root.querySelector('table tbody')
+
         this.update()
     }
 
     update() {
         this.removeAllTr()
 
-        [
-            {
-                login: 'maykbrito',
-                name: "Mayk Brito",
-                public_repos: '76',
-                followers: '120000'
-            }
-        ]
+        this.entries.forEach(user => {
+            const row = this.creatRow()
+
+            row.querySelector('.cardUser img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.cardUser img').alt = `Imagem de ${user.name}`
+            row.querySelector('.cardUser img').title = `Imagem de ${user.name}`
+
+            row.querySelector('.username').textContent = `${user.name}`
+
+            row.querySelector('.login').textContent = `${user.login}`
+
+            row.querySelector('.repositories').textContent = `${user.public_repos}`
+            
+            row.querySelector('.followers').textContent = `${user.followers}`
+
+            this.tbody.append(row)
+        })        
     }
 
     creatRow() {
@@ -30,7 +59,7 @@ export class FavoritesView extends Favorites {
         tr.innerHTML = `
             <td>
                 <div class="cardUser">
-                    <img src="https://avatars.githubusercontent.com/u/107481318?v=4.png" alt="">
+                    <img src="https://avatars.githubusercontent.com/u/107481318?v=4.png" alt="" title="">
                     <a href="" target="_blank">
                         <span class="username">Hudson Diego</span>
                         <p class="login">HudsonSena</p>
@@ -41,15 +70,12 @@ export class FavoritesView extends Favorites {
             <td class="followers">1000</td>
             <td><a href="" class="remove">remover</a></td>
         `
+        return tr
     }
 
     removeAllTr() {
-        const tbody = this.root.querySelector('table tbody')
-        const menssageAdd = document.querySelector('.Nfavorito')
-
-        tbody.querySelectorAll('tr').forEach((tr) => {
+        this.tbody.querySelectorAll('tr').forEach((tr) => {
             tr.remove()
-            menssageAdd.classList.add('displayOn')
         })
     }
 }
