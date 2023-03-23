0388fb0eefc97a856cd4f56cf6e3ca08c933a069
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-03-23 10:40:44 -0300

    att class query params

--- api/src/server.js
+++ api/src/server.js
@@ -11,5 +11,11 @@ app.get("/message/:id/:user", (request, response) => {
     Para o usuário: ${user}`)
 });
 
+app.get("/users", (request, response) => {
+    const { page, limit} = request.query;
+
+    response.send(`Página: ${page}. Mostrar: ${limit}`)
+})
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
