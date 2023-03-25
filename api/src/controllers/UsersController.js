5585d03602ce33b15e543f967c944ac5d30763ed
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-03-25 09:35:51 -0300

    att http codes

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,11 +1,9 @@
-class UserController {
-    /* 
-    --- Pode ser usados até 5 métodos, ou 5 funções ---
-    * index - GET para listar vários registros.
-    * show - GET para exibir um registro específico.
-    * create - POST para criar um registro.
-    * update - PUT para atualizar um registro
-    * delete - DELETE para remover um registro */
+class UsersController {
+    create(request, response){
+        const {name, email, password} = request.body;
+
+        response.status(201).json({name, email, password});
+    }
 };
 
-module.exports - UserController;
+module.exports = UsersController;
