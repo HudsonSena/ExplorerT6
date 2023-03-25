1e7782a79163e195c663e642b697fee44937a53b
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-03-25 11:52:30 -0300

    att to home

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -4,18 +4,8 @@ const UsersController = require("../controllers/UsersController");
 
 const userRoutes = Router();
 
-function myMiddleware(request, response, next) {
-    console.log("Você passou pelo Middleware!");
-
-    if(!request.body.isAdmin) {
-        return response.json({ message: "user unauthorized"})
-    }
-
-    next();
-};
-
 const usersController = new UsersController();
 
-userRoutes.post("/", myMiddleware, usersController.create);
+userRoutes.post("/", usersController.create);
 
 module.exports = userRoutes;
