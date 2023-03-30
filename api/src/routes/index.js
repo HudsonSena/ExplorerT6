233c302d713f567fc2a3d7bdf79c6136184e1604
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-03-30 18:10:53 -0300

    att create notes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,8 +2,12 @@ const { Router } = require("express");
 
 const usersRouter = require("./user.routes");
 
+const notesRouter = require("./notes.routes");
+
 const routes = Router();
 
-routes.use("/users", usersRouter)
+routes.use("/users", usersRouter);
+
+routes.use("/notes", notesRouter);
 
 module.exports = routes;
