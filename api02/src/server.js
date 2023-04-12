29efab31cdaa1b87f232858630197d8e2542a927
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-04-12 16:18:51 -0300

    att project api02

--- api02/src/server.js
+++ api02/src/server.js
@@ -2,5 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3334;
+app.get("/message/id")
+
+const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
