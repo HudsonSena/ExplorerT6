c3065cad9a029f7ea6ce41885af7719b8b603095
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-04-13 11:32:44 -0300

    Update server.js

--- api02/src/server.js
+++ api02/src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/id")
+app.post("/movies", (request, response) => {
+    const { name, description, grade } = request.body;
+
+    response.json({ name, description, grade });
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
