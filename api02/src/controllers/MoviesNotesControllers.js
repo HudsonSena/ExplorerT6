2e03fd7bcacc4c160d18ad4c73e249c0a57f154f
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-04-29 11:56:10 -0300

    update show and index database

--- api02/src/controllers/MoviesNotesControllers.js
+++ api02/src/controllers/MoviesNotesControllers.js
@@ -24,6 +24,36 @@ class MoviesNotesController {
 
         response.json();
     }
+
+    async show(request, response) {
+        const { id } = request.params;
+        
+        const note =  await knex("moviesNotes").where({ id }).first();
+        const tags = await knex("moviesTags").where({ moviesNotes_id: id }).orderBy("name");
+
+        return response.json({
+            ...note,
+            tags
+        });
+    }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("moviesNotes").where({ id }).delete();
+
+        return response.json("Nota deletada");
+    }
+
+    async index(request, response) {
+        const { user_id } = request.query;
+
+        const notes = await knex("moviesNotes")
+        .where({ user_id })
+        .orderBy("title")
+
+        return response.json(notes)
+    }
 }
 
 module.exports = MoviesNotesController;
--- api02/src/routes/moviesNotes.routes.js
+++ api02/src/routes/moviesNotes.routes.js
@@ -6,6 +6,9 @@ const moviesNotesRoutes = Router();
 
 const moviesNotesController = new MoviesNotesController();
 
+moviesNotesRoutes.get("/", moviesNotesController.index);
 moviesNotesRoutes.post("/:user_id", moviesNotesController.create);
+moviesNotesRoutes.get("/:id", moviesNotesController.show);
+moviesNotesRoutes.delete("/:id", moviesNotesController.delete);
 
 module.exports = moviesNotesRoutes;
