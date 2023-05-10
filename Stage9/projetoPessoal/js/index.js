f800fa1fda5d86b39f39e71dc203d505463193b1
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-05-10 16:14:42 -0300

    update nav

--- Stage9/projetoPessoal/js/index.js
+++ Stage9/projetoPessoal/js/index.js
@@ -8,6 +8,8 @@ router.add("/clinics", "pages/clinics.html")
 router.add("/login", "pages/login.html")
 router.add("/professionals", "pages/professionals.html")
 router.add("/register", "pages/register.html")
+router.add("/chat", "pages/chat.html")
+router.add("/forum", "pages/forum.html")
 router.add(404, "pages/404.html")
 
 router.handle()
@@ -21,31 +23,5 @@ const btnProfessionals = document.querySelector('.btnProfessionals')
 const btnAbout = document.querySelector('.btnAbout')
 const btnLogin = document.querySelector('.btnLogin')
 const btnRegister = document.querySelector('.btnRegister')
-
-btHome.addEventListener('click', bgHome)
-function bgHome() {
-    bgBody.classList.add('bgHome')
-    bgBody.classList.remove('bgUniverse')
-    bgBody.classList.remove('bgExploracao')
-    btHome.classList.add('textcolor')
-    btExploracao.classList.remove('textcolor')
-    btUniverse.classList.remove('textcolor')
-}
-btUniverse.addEventListener('click', bgUniverse)
-function bgUniverse() {
-    bgBody.classList.remove('bgHome')
-    bgBody.classList.add('bgUniverse')
-    bgBody.classList.remove('bgExploracao')
-    btHome.classList.remove('textcolor')
-    btExploracao.classList.remove('textcolor')
-    btUniverse.classList.add('textcolor')
-}
-btExploracao.addEventListener('click', bgExploracao)
-function bgExploracao() {
-    bgBody.classList.remove('bgHome')
-    bgBody.classList.remove('bgUniverse')
-    bgBody.classList.add('bgExploracao')
-    btHome.classList.remove('textcolor')
-    btExploracao.classList.add('textcolor')
-    btUniverse.classList.remove('textcolor')
-}
+const btnChat = document.querySelector('.btnChat')
+const btnForum = document.querySelector('.btnForum')
