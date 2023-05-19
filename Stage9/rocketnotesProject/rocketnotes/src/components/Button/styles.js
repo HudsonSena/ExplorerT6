2fa3fc5adeee5d18257a5c190ca699dc331c9bb3
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-05-19 16:09:26 -0300

    update button and propriets

--- Stage9/rocketnotesProject/rocketnotes/src/components/Button/styles.js
+++ Stage9/rocketnotesProject/rocketnotes/src/components/Button/styles.js
@@ -8,6 +8,12 @@ export const Container = styled.button`
     height: 56px;
     border: 0;
     padding: 0 16px;
-    &:disabled;
+    margin-top: 16px;
+    border-radius: 10px;
+    font-weight: 500;
+
+    &:disabled {
+        opacity: 0.5;
+    };
 
 `;
--- Stage9/rocketnotesProject/rocketnotes/src/pages/Details/index.jsx
+++ Stage9/rocketnotesProject/rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,17 @@
 import { Container } from "./styles";
 
+import { Button } from "../../components/Button";
+
 export function Details(){
 
   return(
     <Container>
       <h1>Hello World</h1>
+      <span> Hudson Diego</span>
+
+      <Button title="Entrar" loading ></Button>
+      <Button title="Cadastrar"></Button>
+      <Button title="Voltar"></Button>
     </Container>
   )
 }
