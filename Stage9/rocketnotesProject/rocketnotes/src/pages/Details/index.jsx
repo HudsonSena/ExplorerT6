0b41ff5ec1225997af397f5bb5dd9dcd63d882e1
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-05-29 16:50:13 -0300

    update pages

--- Stage9/rocketnotesProject/rocketnotes/src/pages/Details/index.jsx
+++ Stage9/rocketnotesProject/rocketnotes/src/pages/Details/index.jsx
@@ -1,8 +1,10 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 import { Header} from '../../components/Header';
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details(){
 
@@ -10,15 +12,34 @@ export function Details(){
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="">https://www.rocketseat.com.br</a></li>
-          <li><a href="">https://www.rocketseat.com.br</a></li>
-          <li><a href="">https://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota"/>
 
-      <Button title="Voltar" />
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex recusandae illo odio vel harum quos vitae blanditiis rerum dignissimos dolorem ipsa, incidunt dicta enim doloribus! Fugiat aperiam a quibusdam facilis.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati hic commodi ab pariatur repellendus totam, eligendi labore adipisci magni eum recusandae dolorem dolore itaque architecto quos esse distinctio a officiis?
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="">https://www.rocketseat.com.br</a></li>
+              <li><a href="">https://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
