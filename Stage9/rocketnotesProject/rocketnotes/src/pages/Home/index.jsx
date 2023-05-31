97eac1db87f6469f28148920e3003748dff51d68
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-05-31 16:52:04 -0300

    update input

--- Stage9/rocketnotesProject/rocketnotes/src/pages/Home/index.jsx
+++ Stage9/rocketnotesProject/rocketnotes/src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
+import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
 import { ButtonText} from '../../components/ButtonText';
 
 export function Home() {
@@ -25,12 +27,15 @@ export function Home() {
                 </Menu>
 
                 <Search>
+                    <Input placeholder="Pesquisar pelo título" />
                 </Search>
 
                 <Content>
                 </Content>
 
-                <NewNote>                    
+                <NewNote>
+                    <FiPlus />
+                    Criar Notas
                 </NewNote>
             </Container>
         )
