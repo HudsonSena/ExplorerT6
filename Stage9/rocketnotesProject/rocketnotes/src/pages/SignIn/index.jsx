ced8b20376d41be30495ccc4cee024e6ade2e0a5
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-06-07 16:26:30 -0300

    update singIn

--- Stage9/rocketnotesProject/rocketnotes/src/pages/SignIn/index.jsx
+++ Stage9/rocketnotesProject/rocketnotes/src/pages/SignIn/index.jsx
@@ -1,8 +1,34 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+
+import { Input } from "../../components/Input";
+import { FiMail, FiLock } from 'react-icons/fi';
+import { Button } from '../../components/Button';
 
 export function SignIn(){
     return (
         <Container>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input
+                    placehoulder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placehoulder="E-mail"
+                    type="text"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar"/>
+
+                <a href="#">Criar Conta</a>
+            </Form>
 
         </Container>        
     );
