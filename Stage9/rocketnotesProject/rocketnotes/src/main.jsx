1cb592d0b29f4deb7b02122c7c54a6ea2d7f682e
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-06-07 11:25:00 -0300

    update signin

--- Stage9/rocketnotesProject/rocketnotes/src/main.jsx
+++ Stage9/rocketnotesProject/rocketnotes/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Home } from './pages/Home/index.jsx';
+import { SignIn } from './pages/SignIn/index.jsx';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme.js';
 import GlobalStyle from './styles/global.js'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
