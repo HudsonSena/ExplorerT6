810a0a04a2e7daa5a762f8760498429b0f70a892
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-06-13 11:37:50 -0300

    update page new

--- Stage9/rocketnotesProject/rocketnotes/src/main.jsx
+++ Stage9/rocketnotesProject/rocketnotes/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Profile } from './pages/Profile/index.jsx';
+import { New } from './pages/New/index.jsx';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme.js';
 import GlobalStyle from './styles/global.js'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
