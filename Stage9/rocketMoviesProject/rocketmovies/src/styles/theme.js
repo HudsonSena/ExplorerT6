2b59741b5dfdd45235c564a6efa4274d0f98a6f2
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-06-15 20:56:20 -0300

    update themes

---
+++ Stage9/rocketMoviesProject/rocketmovies/src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+        background01: "#1C1B1E",
+        background02: "#262529",
+        background03: "#282124",
+        background04: "#312E38",
+
+        white01: "#FFFFFF",
+        white02: "#F4EDE8",
+        white03: "#E5E5E5",
+        
+        grey: "#999591",
+        pink: "#FF859B",
+    }
+};
