0b563b5f8ab1f67793a2943623fbb2cfa52c1f8a
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-07-10 16:52:09 -0300

    update sessions authenticated

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,6 +6,8 @@ const notesRouter = require("./notes.routes");
 
 const tagsRouter = require("./tags.routes");
 
+const sessionsRouter = require("./sessions.routes");
+
 const routes = Router();
 
 routes.use("/users", usersRouter);
@@ -13,5 +15,7 @@ routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
 
 routes.use("/tags", tagsRouter);
+
+routes.use("/sessions", sessionsRouter)
 
 module.exports = routes;
