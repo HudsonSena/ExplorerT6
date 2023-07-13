f9767a380695357de4c656f509e5e6c74d3ba07e
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-07-13 16:42:43 -0300

    update upload and past tmp

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const userRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
 userRoutes.post("/", usersController.create);
 userRoutes.put("/", ensureAuthenticated, usersController.update);
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+    console.log(request.file.filename);
+    response.json();
+})
 
 module.exports = userRoutes;
