065f2d54cb16f930937d8a07f51528713c6c9352
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-07-15 11:58:07 -0300

    update avatar padate and view

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const multer = require("multer");
 const uploadConfig = require("../configs/upload");
+const UsersAvatarController = require("../controllers/UsersAvatarController");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
@@ -9,12 +10,10 @@ const userRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
 
 userRoutes.post("/", usersController.create);
 userRoutes.put("/", ensureAuthenticated, usersController.update);
-userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
-    console.log(request.file.filename);
-    response.json();
-})
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), usersAvatarController.update)
 
 module.exports = userRoutes;
