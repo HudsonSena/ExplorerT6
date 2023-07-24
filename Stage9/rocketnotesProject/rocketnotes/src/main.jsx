c08ff02c6c6e7781bdbb423e7d4d753d825a1f22
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-07-24 17:01:03 -0300

    update auth provider in page signIn

--- Stage9/rocketnotesProject/rocketnotes/src/main.jsx
+++ Stage9/rocketnotesProject/rocketnotes/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Routes } from './routes';
 import { ThemeProvider } from 'styled-components';
+import { AuthProvider } from "./hooks/auth";
 import theme from './styles/theme.js';
 import GlobalStyle from './styles/global.js'
 
@@ -9,8 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
--- Stage9/rocketnotesProject/rocketnotes/src/pages/SignIn/index.jsx
+++ Stage9/rocketnotesProject/rocketnotes/src/pages/SignIn/index.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import { Container, Form, Background } from './styles.js';
 import { Link} from 'react-router-dom';
 import { Input } from "../../components/Input";
 import { FiMail, FiLock } from 'react-icons/fi';
 import { Button } from '../../components/Button';
+import { useAuth } from '../../hooks/auth';
 
 export function SignIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { signIn } = useAuth();
+
+    function handleSignIn() {
+        signIn({ email, password });
+    }
+
     return (
         <Container>
             <Form>
@@ -17,15 +28,17 @@ export function SignIn(){
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder="Senha"
-                    type="text"
+                    type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Entrar"/>
+                <Button title="Entrar" onClick={handleSignIn} />
 
                 <Link to="/register">Criar nota</Link>
             </Form>
