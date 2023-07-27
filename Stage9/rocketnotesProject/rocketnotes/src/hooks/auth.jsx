27c80ed1d96e38757837966087b9a061f29e6b05
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-07-27 16:46:10 -0300

    Update auth.jsx

--- Stage9/rocketnotesProject/rocketnotes/src/hooks/auth.jsx
+++ Stage9/rocketnotesProject/rocketnotes/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../../../../../api/src/services/api";
 
@@ -24,6 +24,20 @@ function AuthProvider({ children }) {
             }
         }
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketnotes: token");
+        const user = localStorage.getItem("@rocketnotes: user");
+
+        if(token && user) {
+            api.defaults.headers.authorization = `Bearer ${token}`;
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+    })
 
     return (
         <AuthContext.Provider value={{ signIn, user: data.user }}>
