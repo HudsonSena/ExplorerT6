13147448c0bf593e766d84570b9f478bad124497
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-09-05 14:48:03 -0300

    update theme

---
+++ ProjetoFinal/FrontEndFoodExplorerSena/src/pages/Details/Index.jsx
@@ -0,0 +1,7 @@
+import { Container } from './styles';
+
+export function Details() {
+  return(
+    <Container><h1>Hello World</h1></Container>
+  )
+};
---
+++ ProjetoFinal/FrontEndFoodExplorerSena/src/styles/theme.js
@@ -0,0 +1,23 @@
+export default {
+    colors: {
+        backgroundPages: "#000A0F",
+        backgroundHeader: "#001119",
+        backgroundFooter: "#001119",
+        backgroundForm: "#001119",
+        backgroundCard: "#00070A",
+        backgroundInput: "#0D1D25",
+        backgroundButton: "#750310",
+        backgroundButtonHover: "#92000E",
+        backgroundButtonInert: "#AB4D55",
+
+        textLogo: "#FFFFFF",
+        textDescription: "#E1E1E6",
+        textTitleFooter: "#4D585E",
+        textDescriptionFooter: "#FFFAF1",
+        textLabel: "#C4C4CC",
+        textPlaceholder: "#7C7C8A",
+        textButton1: "#FFFFFF",
+        textButton2: "#E1E1E6",
+        textCost: "#82F3FF",
+    }
+};
