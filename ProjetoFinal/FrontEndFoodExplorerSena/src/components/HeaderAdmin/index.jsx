ca241bea242a22b04416f0c5367fd06eb224b959
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-09-25 11:43:43 -0300

    update menu burguer continued

--- ProjetoFinal/FrontEndFoodExplorerSena/src/components/HeaderAdmin/index.jsx
+++ ProjetoFinal/FrontEndFoodExplorerSena/src/components/HeaderAdmin/index.jsx
@@ -7,21 +7,30 @@ import { Link } from 'react-router-dom';
 export function HeaderAdmin() {
     return(
         <Container>
-            <div>
-                <svg width="26" height="31" viewBox="0 0 26 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13 0.866394L25.9904 8.36639V23.3664L13 30.8664L0.00961876 23.3664V8.36639L13 0.866394Z" fill="#065E7C"/>
-                </svg>
+            <div className='menuclassic'>
                 <div>
-                    <h1>food explorer</h1>
-                    <span className='admin'>admin</span>
+                    <svg width="26" height="31" viewBox="0 0 26 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13 0.866394L25.9904 8.36639V23.3664L13 30.8664L0.00961876 23.3664V8.36639L13 0.866394Z" fill="#065E7C"/>
+                    </svg>
+                    <div>
+                        <h1>food explorer</h1>
+                        <span className='admin'>admin</span>
+                    </div>
+                </div>
+                
+                <Input placeholder="Busque por pratos ou ingredientes" icon={FiSearch} type="text"/>
+                
+                <Button title="Novo prato"></Button>
+                <Link><FiLogOut /></Link>
+            </div>
+            <div className='menuburguer'>
+                <Link>Menu</Link>
+                <div className='menulist'>
+                    <Input placeholder="Busque por pratos ou ingredientes" icon={FiSearch} type="text"/>
+                    <Link>Novo prato</Link>
+                    <Link>Sair</Link>
                 </div>
             </div>
-            
-            <Input placeholder="Busque por pratos ou ingredientes" icon={FiSearch} type="text"/>
-            
-            <Button title="Novo prato"></Button>
-
-            <Link><FiLogOut /></Link>
         </Container>
     )
 }
--- ProjetoFinal/FrontEndFoodExplorerSena/src/components/HeaderAdmin/styles.js
+++ ProjetoFinal/FrontEndFoodExplorerSena/src/components/HeaderAdmin/styles.js
@@ -2,59 +2,126 @@ import { styled } from 'styled-components';
 
 export const Container = styled.header`
     grid-area: header;
-    
-    width: 100%;
-    height: 10.4rem;
-    background-color: ${({ theme }) => theme.colors.bgHeader};
 
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    gap: 3.2rem;
+    .menuclassic {
+        width: 100%;
+        height: 10.4rem;
+        background-color: ${({ theme }) => theme.colors.bgHeader};
 
-    padding: 1rem 12.3rem;
-    
-    position: fixed;
-
-    z-index: 2;
-
-    button {
-        width: auto;
-    }
-
-    > div {
         display: flex;
-        gap: 1rem;
-        min-width: 21rem;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 3.2rem;
 
-        h1 {
-            font-size: 2.8rem;
+        padding: 1rem 12.3rem;
+        
+        position: fixed;
+
+        z-index: 2;
+
+        button {
+            width: auto;
+        }
+
+        > div {
+            display: flex;
+            gap: 1rem;
+            min-width: 21rem;
+
+            h1 {
+                font-size: 2.8rem;
+            }
+
+            div {
+                display: flex;
+                flex-direction: column;
+                align-items: flex-end;
+                line-height: 1;
+            }
+
+            span {
+                color: ${({ theme }) => theme.colors.txtCostAdm};
+                font-size: 1.2rem;
+            }
+        }
+
+        > img {
+            width: 5.6rem;
+            height: 5.6rem;
+            border-radius: 50%;
+        }
+
+        > a {
+            color: ${({ theme }) => theme.colors.txtBtn1};
+            font-size: 2.2rem;
+            display: flex;
+            align-items: center;
+        }
+
+        @media (max-width: 920px) {
+            display: none;
+        }
+    }
+    
+    .menuburguer {
+        width: 100%;
+        height: 11.4rem;
+        background-color: ${({ theme }) => theme.colors.bgHeader};
+
+        a {
+            color: ${({ theme }) => theme.colors.txtBtn1};
+            font-family: 'Roboto';
+            font-size: 2.1rem;
         }
 
         div {
             display: flex;
             flex-direction: column;
-            align-items: flex-end;
-            line-height: 1;
+
+            input {
+                width: 100%;
+            }
         }
 
-        span {
-            color: ${({ theme }) => theme.colors.txtCostAdm};
-            font-size: 1.2rem;
+        .menulist {
+            width: 100%;
+            height: auto;
+            margin-top: .8rem;
+            padding: 2.8rem;
+
+            position: absolute;
+
+            display: flex;
+            flex-direction: column;
+            gap: 3.6rem;
+
+            background-color: ${({ theme }) => theme.colors.bgPages};
+
+            div {
+                display: flex;
+                flex-direction: row;
+
+                margin: auto;
+                gap: 1rem;
+            }
+
+            a {
+                font-family: 'Poppins';
+                font-size: 2.4rem;
+                font-weight: 200;
+
+                border-bottom: .05rem solid ${({ theme}) => theme.colors.bgIpt01};
+
+                padding: 1rem;
+            }
+            
+            
         }
     }
+    
 
-    > img {
-        width: 5.6rem;
-        height: 5.6rem;
-        border-radius: 50%;
-    }
+    @media (max-width: 920px) {
 
-    > a {
-        color: ${({ theme }) => theme.colors.txtBtn1};
-        font-size: 2.2rem;
-        display: flex;
-        align-items: center;
     }
 `;
