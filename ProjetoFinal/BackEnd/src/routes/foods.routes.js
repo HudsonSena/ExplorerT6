4718f5217ff5b1f3b8e64c469ff283d27f8f0647
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-02-27 16:50:31 -0300

    update foodimage uploads

--- ProjetoFinal/BackEnd/src/routes/foods.routes.js
+++ ProjetoFinal/BackEnd/src/routes/foods.routes.js
@@ -1,11 +1,17 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const FoodsController = require("../controllers/FoodsController");
+const FoodsImageController = require("../controllers/FoodsImageController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const foodsController = new FoodsController();
+//const foodsImageController = new FoodsImageController();
 
 const foodsRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
+
 foodsRoutes.get("/", foodsController.index);
 foodsRoutes.get("/:id", foodsController.show)
 
@@ -13,7 +19,10 @@ foodsRoutes.use(ensureAuthenticated)
 
 foodsRoutes.post("/", foodsController.create);
 foodsRoutes.delete("/", foodsController.delete);
-
+foodsRoutes.patch("/foodimage", upload.single("foodimage"),(request, response) => {
+    console.log(request.file.filename);
+    response.json();
+});
 foodsRoutes.put("/", foodsController.update);
 
 module.exports = foodsRoutes;
