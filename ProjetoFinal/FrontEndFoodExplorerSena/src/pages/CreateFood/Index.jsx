628ab9edff2b9af29760e9baa30a8fbb0f499adb
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-04-30 21:32:16 -0300

    update foodregister ok

--- ProjetoFinal/FrontEndFoodExplorerSena/src/pages/CreateFood/Index.jsx
+++ ProjetoFinal/FrontEndFoodExplorerSena/src/pages/CreateFood/Index.jsx
@@ -9,7 +9,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
 import { HeaderAdmin } from "../../components/HeaderAdmin";
 import { api } from "../../services/api";
-import { useAuth } from "../../hooks/auth";
+import { FiUpload } from "react-icons/fi";
 
 export function CreateFood() {
   const [title, setTitle] = useState("");
@@ -41,25 +41,45 @@ export function CreateFood() {
   }
 
   async function handleNewFood() {
-    if (!title) {
-      return alert("Você deixou o título em Branco!");
+    try {
+      if (!foodImageFile) {
+        return alert("Você deixou o título em Branco!");
+      }
+
+      if (!title) {
+        return alert("Você deixou o título em Branco!");
+      }
+
+      if (!category) {
+        return alert("Você não escolheu a categoria!");
+      }
+
+      if (newTag) {
+        return alert("Você deixou um ingrediente em branco ou não adicionou!");
+      }
+
+      if (!cost) {
+        return alert("Você não colocou o valor do prato!");
+      }
+
+      const fileUploadForm = new FormData();
+      fileUploadForm.append("foodimage", foodImageFile);
+      fileUploadForm.append("title", title);
+      fileUploadForm.append("category", category);
+      fileUploadForm.append("description", description);
+      tags.forEach((tag) => fileUploadForm.append("tags[]", tag));
+      fileUploadForm.append("cost", cost);
+
+      await api.post("/foods", fileUploadForm);
+
+      alert("Prato cadastrado com sucesso!");
+      navigate("/");
+    } catch (error) {
+      console.error("Erro ao cadastrar prato:", error);
+      alert(
+        "Erro ao cadastrar prato. Verifique os detalhes e tente novamente."
+      );
     }
-
-    if (newTag) {
-      return alert("Você deixou um ingrediente em branc0 ou não adicionou!");
-    }
-
-    await api.post("/foods", {
-      title,
-      category,
-      foodimage: foodImageFile,
-      tags,
-      cost,
-      description,
-    });
-
-    alert("Prato cadastrado com sucesso!");
-    navigate("/");
   }
 
   return (
@@ -80,32 +100,7 @@ export function CreateFood() {
               <section>
                 <img src={foodImage} />
                 <Avatar>
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M11.2929 0.292893C11.6834 -0.0976311 12.3166 -0.0976311 12.7071 0.292893L17.9571 5.54289C18.3476 5.93342 18.3476 6.56658 17.9571 6.95711C17.5666 7.34763 16.9334 7.34763 16.5429 6.95711L12 2.41421L7.45711 6.95711C7.06658 7.34763 6.43342 7.34763 6.04289 6.95711C5.65237 6.56658 5.65237 5.93342 6.04289 5.54289L11.2929 0.292893Z"
-                      fill="white"
-                    />
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M12 1.49012e-08C12.5523 1.49012e-08 13 0.447715 13 1V15C13 15.5523 12.5523 16 12 16C11.4477 16 11 15.5523 11 15V1C11 0.447715 11.4477 1.49012e-08 12 1.49012e-08Z"
-                      fill="white"
-                    />
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M1 14C1.55228 14 2 14.4477 2 15V22H22V15C22 14.4477 22.4477 14 23 14C23.5523 14 24 14.4477 24 15V22C24 22.5304 23.7893 23.0391 23.4142 23.4142C23.0391 23.7893 22.5304 24 22 24H2C1.46957 24 0.960861 23.7893 0.585787 23.4142C0.210714 23.0391 0 22.5304 0 22V15C0 14.4477 0.447715 14 1 14Z"
-                      fill="white"
-                    />
-                  </svg>
+                  <FiUpload size={30} />
                   <label htmlFor="inputImg">Selecione imagem</label>
                   <input
                     id="inputImg"
@@ -143,13 +138,6 @@ export function CreateFood() {
             <div>
               <label htmlFor="inputIngred">Ingredientes</label>
               <div className="newTags">
-                {tags.map((tag, index) => (
-                  <FoodItem
-                    key={String(index)}
-                    value={tag}
-                    onClick={() => handleRemoveTag(tag)}
-                  />
-                ))}
                 <FoodItem
                   isNew
                   placeholder="Adicionar"
@@ -157,6 +145,13 @@ export function CreateFood() {
                   value={newTag}
                   onClick={handleAddTag}
                 />
+                {tags.map((tag, index) => (
+                  <FoodItem
+                    key={String(index)}
+                    value={tag}
+                    onClick={() => handleRemoveTag(tag)}
+                  />
+                ))}
               </div>
             </div>
             <div className="sectioncost">
