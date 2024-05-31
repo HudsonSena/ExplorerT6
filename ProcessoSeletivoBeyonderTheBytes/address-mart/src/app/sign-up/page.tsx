f46540eadc7ee3d483dabe78e3c17254915e256d
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-05-31 11:55:04 -0300

    update in project

---
+++ ProcessoSeletivoBeyonderTheBytes/address-mart/src/app/sign-up/page.tsx
@@ -0,0 +1,248 @@
+import { SignUpForm } from "./sign-up-form";
+
+export default function signUp() {
+  return (
+    <main className="flex gap-2 min-h-screen bg-slate-200 p-10">
+      <div className="flex flex-col items-center">
+        <SignUpForm />
+      </div>
+      <div className="flex flex-wrap gap-2 w-screen h-min border border-zinc-300 rounded-xl p-2 text-xs">
+        <div className="cursor-pointer flex flex-col rounded-xl border bg-card text-card-foreground shadow p-5 h-min">
+          <ul>
+            <li>
+              <span className="font-semibold text-zinc-00">Nome:</span> Maria de
+              Souza
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Número de Telefone:
+              </span>{" "}
+              99-999-999-999
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Planeta:</span>{" "}
+              Terra
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Endereço/Lote:{" "}
+              </span>
+              Camara Cascudo
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Residência: </span>
+              Casa
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Número:</span> 1000
+            </li>
+          </ul>
+        </div>
+        <div className="cursor-pointer flex flex-col rounded-xl border bg-card text-card-foreground shadow p-5 h-min">
+          <ul>
+            <li>
+              <span className="font-semibold text-zinc-00">Nome:</span> José de
+              Souza
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Número de Telefone:
+              </span>{" "}
+              99-999-999-999
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Planeta:</span>{" "}
+              Marte
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Endereço/Lote:
+              </span>{" "}
+              9999
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Residência:</span> -
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Número:</span> -
+            </li>
+          </ul>
+        </div>
+        <div className="cursor-pointer flex flex-col rounded-xl border bg-card text-card-foreground shadow p-5 h-min">
+          <ul>
+            <li>
+              <span className="font-semibold text-zinc-00">Nome:</span> Maria de
+              Souza
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Número de Telefone:
+              </span>{" "}
+              99-999-999-999
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Planeta:</span>{" "}
+              Terra
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Endereço/Lote:
+              </span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Residência:</span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Número:</span>
+            </li>
+          </ul>
+        </div>
+        <div className="cursor-pointer flex flex-col rounded-xl border bg-card text-card-foreground shadow p-5 h-min">
+          <ul>
+            <li>
+              <span className="font-semibold text-zinc-00">Nome:</span> Maria de
+              Souza
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Número de Telefone:
+              </span>{" "}
+              99-999-999-999
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Planeta:</span>{" "}
+              Terra
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Endereço/Lote:
+              </span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Residência:</span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Número:</span>
+            </li>
+          </ul>
+        </div>
+        <div className="cursor-pointer flex flex-col rounded-xl border bg-card text-card-foreground shadow p-5 h-min">
+          <ul>
+            <li>
+              <span className="font-semibold text-zinc-00">Nome:</span> Maria de
+              Souza
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Número de Telefone:
+              </span>{" "}
+              99-999-999-999
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Planeta:</span>{" "}
+              Terra
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Endereço/Lote:
+              </span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Residência:</span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Número:</span>
+            </li>
+          </ul>
+        </div>
+        <div className="cursor-pointer flex flex-col rounded-xl border bg-card text-card-foreground shadow p-5 h-min">
+          <ul>
+            <li>
+              <span className="font-semibold text-zinc-00">Nome:</span> Maria de
+              Souza
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Número de Telefone:
+              </span>{" "}
+              99-999-999-999
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Planeta:</span>{" "}
+              Terra
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Endereço/Lote:
+              </span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Residência:</span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Número:</span>
+            </li>
+          </ul>
+        </div>
+        <div className="cursor-pointer flex flex-col rounded-xl border bg-card text-card-foreground shadow p-5 h-min">
+          <ul>
+            <li>
+              <span className="font-semibold text-zinc-00">Nome:</span> Maria de
+              Souza
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Número de Telefone:
+              </span>{" "}
+              99-999-999-999
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Planeta:</span>{" "}
+              Terra
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Endereço/Lote:
+              </span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Residência:</span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Número:</span>
+            </li>
+          </ul>
+        </div>
+        <div className="cursor-pointer flex flex-col rounded-xl border bg-card text-card-foreground shadow p-5 h-min">
+          <ul>
+            <li>
+              <span className="font-semibold text-zinc-00">Nome:</span> Maria de
+              Souza
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Número de Telefone:
+              </span>{" "}
+              99-999-999-999
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Planeta:</span>{" "}
+              Terra
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">
+                Endereço/Lote:
+              </span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Residência:</span>
+            </li>
+            <li>
+              <span className="font-semibold text-zinc-700">Número:</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
