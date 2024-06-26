0ae2a27284bc563bdecf23ba6e15f4fc28b72b33
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-06-26 17:14:58 -0300

    update "update food"

--- ProjetoFinal/FrontEndFoodExplorerSena/src/pages/UpdateFood/Index.jsx
+++ ProjetoFinal/FrontEndFoodExplorerSena/src/pages/UpdateFood/Index.jsx
@@ -13,19 +13,11 @@ import foodPlaceholder from "../../assets/foodimage_placeholder.svg";
 import { FiUpload } from "react-icons/fi";
 
 export function UpdateFood() {
-  const [tags, setTags] = useState([]);
-  const [newTag, setNewTag] = useState("");
-  const [newCost, setNewCost] = useState();
-  const [newDescription, setNewDescription] = useState();
-  const [data, setData] = useState("");
-  const params = useParams();
-  const [foodImage, setFoodImage] = useState(foodPlaceholder);
-  const [foodImageFile, setFoodImageFile] = useState(null);
-
   useEffect(() => {
     async function fetchNote() {
       const response = await api.get(`/foods/${params.id}`);
       setData(response.data);
+
       if (response.data.foodimage) {
         setFoodImage(`${api.defaults.baseURL}/files/${response.data.foodimage}`);
       }
@@ -34,7 +26,15 @@ export function UpdateFood() {
     fetchNote();
   }, []);
 
+  const [data, setData] = useState(null);
+  const params = useParams();
 
+  const [newCategory, setNewCategory] = useState("")
+  const [newTag, setNewTag] = useState("");
+  const [newCost, setNewCost] = useState("");
+  const [newDescription, setNewDescription] = useState();
+  const [foodImage, setFoodImage] = useState(foodPlaceholder);
+  const [foodImageFile, setFoodImageFile] = useState(null);
 
   function handleChangeImgFood(event) {
     const file = event.target.files[0];
@@ -56,8 +56,8 @@ export function UpdateFood() {
   async function handleUpdateFood() {
     const food = {
       title,
-      category,
-      tags,
+      category: newCategory,
+      tags: newTag,
       cost: newCost,
       description: newDescription,
     };
@@ -76,92 +76,94 @@ export function UpdateFood() {
 
         <h1>Editar prato</h1>
 
-        <Form>
-          <div className="info01">
-            <div>
-              <label htmlFor="inputImg">Imagem do prato</label>
-              <section>
-                <img src={foodImage} />
-                <Avatar>
-                  <FiUpload size={30} />
-                  <label htmlFor="inputImg">Selecione imagem</label>
-                  <input
-                    id="inputImg"
-                    type="file"
-                    onChange={handleChangeImgFood}
-                  />
-                </Avatar>
-              </section>
+        {data && (
+          <Form>
+            <div className="info01">
+              <div>
+                <label htmlFor="inputImg">Imagem do prato</label>
+                <section>
+                  <img src={foodImage} />
+                  <Avatar>
+                    <FiUpload size={30} />
+                    <label htmlFor="inputImg">Selecione imagem</label>
+                    <input
+                      id="inputImg"
+                      type="file"
+                      onChange={handleChangeImgFood}
+                    />
+                  </Avatar>
+                </section>
+              </div>
+              <div>
+                <label htmlFor="inputName">Nome</label>
+                <Input
+                  id="inputName"
+                  type="text"
+                  placeholder={data.title}
+                  disabled
+                />
+              </div>
+              <div>
+                <label htmlFor="inputCategory">Categoria</label>
+                <Input id="inputCategory" type="search" list="category" placeholder={data.category} />
+                <datalist id="category">
+                  <option value="Refeições" />
+                  <option value="Sobremesas" />
+                  <option value="Bebidas" />
+                </datalist>
+              </div>
             </div>
-            <div>
-              <label htmlFor="inputName">Nome</label>
-              <Input
-                id="inputName"
-                type="text"
-                placeholder="Ex.: Salada Ceasar"
-              />
-            </div>
-            <div>
-              <label htmlFor="inputCategory">Categoria</label>
-              <Input id="inputCategory" type="search" list="category" />
-              <datalist id="category">
-                <option value="Refeições" />
-                <option value="Sobremesas" />
-                <option value="Bebidas" />
-              </datalist>
-            </div>
-          </div>
-          <div className="info02">
-            <div>
-              <label htmlFor="inputIngred">Ingredientes</label>
-              <div className="newTags">
-                {tags.map((tag, index) => (
+            <div className="info02">
+              <div>
+                <label htmlFor="inputIngred">Ingredientes</label>
+                <div className="newTags">
+                  {data.tags.map((tag, index) => (
+                    <FoodItem
+                      key={String(index)}
+                      value={tag}
+                      onClick={() => handleRemoveTag(tag)}
+                    />
+                  ))}
                   <FoodItem
-                    key={String(index)}
-                    value={tag}
-                    onClick={() => handleRemoveTag(tag)}
+                    isNew
+                    placeholder="Adicionar"
+                    onChange={(e) => setNewTag(e.target.value)}
+                    onClick={handleAddTag}
                   />
-                ))}
-                <FoodItem
-                  isNew
-                  placeholder="Adicionar"
-                  onChange={(e) => setNewTag(e.target.value)}
-                  value={newTag}
-                  onClick={handleAddTag}
+                </div>
+              </div>
+              <div className="sectioncost">
+                <label htmlFor="inputCost">Preço</label>
+                <Input
+                  id="inputCost"
+                  type="number"
+                  placeholder={`R$ ${data.cost}`}
+                  step="0.00"
+                  min="0.00"
+                  max="10000.00"
+                  onChange={setNewCost}
                 />
               </div>
             </div>
-            <div className="sectioncost">
-              <label htmlFor="inputCost">Preço</label>
-              <Input
-                id="inputCost"
-                type="number"
-                placeholder="R$ 00,00"
-                step="0.00"
-                min="0.00"
-                max="10000.00"
-                onChange={setNewCost}
+            <div className="info03">
+              <label htmlFor="inputDescription">Descrição</label>
+              <Textarea
+                id="inputDescription"
+                placeholder={data.description}
+                onChange={(e) => setNewDescription(e.target.value)}
               />
             </div>
-          </div>
-          <div className="info03">
-            <label htmlFor="inputDescription">Descrição</label>
-            <Textarea
-              id="inputDescription"
-              placeholder="Fale brevemente sobre o prato, seus ingredientes e composição."
-              onChange={setNewDescription}
-            />
-          </div>
-          <div className="deleteUpdate">
-            <Button title="Excluir prato" id="deleteFood" />
-            <Button
-              title="Salvar Alterações"
-              disabled={true}
-              id="addFood"
-              onClick={handleUpdateFood}
-            />
-          </div>
-        </Form>
+            <div className="deleteUpdate">
+              <Button title="Excluir prato" id="deleteFood" />
+              <Button
+                title="Salvar Alterações"
+                disabled={true}
+                id="addFood"
+                onClick={handleUpdateFood}
+              />
+            </div>
+          </Form>
+        )}
       </Content>
       <Footer />
     </Container>
