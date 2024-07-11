53b3a2d88bd94c1c6808a0e369907bd01f31aea6
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-07-11 16:33:26 -0300

    update final before deploy

--- ProjetoFinal/BackEnd/src/server.js
+++ ProjetoFinal/BackEnd/src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
@@ -17,8 +18,8 @@ app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
-app.use(( error, request, response, next ) => {
-    if(error instanceof AppError) {
+app.use((error, request, response, next) => {
+    if (error instanceof AppError) {
         return response.status(error.statusCode).json({
             status: "error",
             message: error.message
@@ -33,5 +34,5 @@ app.use(( error, request, response, next ) => {
     });
 });
 
-const PORT = 3333;
+const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
